a9a55172e1ad5eb5400052b530e890f995d2a4c4
gccpsben <gccpsben@gmail.com>
2024-07-13 00:28:38 +0800

    Improved run_prod.bat

--- run_prod.bat
+++ run_prod.bat
@@ -1,1 +1,3 @@
-wt.exe -w 0 -d "." cmd /k "cd ./server && npm run run_prod"
+wt -w 0 nt --title "Finance Manager" -- ^
+ "%PROGRAMFILES%\Git\usr\bin\bash.exe" -i -l -c ^
+ "cd ./server && npm run run_prod && read -rsp $'Press enter to exit uwu'"
