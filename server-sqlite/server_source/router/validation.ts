3c123f02b9ea7f735075935f84b672a80285c0c0
gccpsben <gccpsben@gmail.com>
2024-07-24 17:11:12 +0800

    Added validation from plain to class

--- server-sqlite/server_source/router/validation.ts
+++ server-sqlite/server_source/router/validation.ts
@@ -1,15 +1,29 @@
-import { validate } from "class-validator";
+import { ClassConstructor, plainToInstance } from "class-transformer";
+import { validate, ValidationError } from "class-validator";
+
+/**
+ * This error represents a validation erorr happening in the internal logics of the server,
+ * and not just incorrect RESTful requests.
+ * If this happens, that means there's probably something wrong with the server itself.
+ */
+export class InternalValidationError extends Error
+{
+    public classValidatorError: ValidationError;
+    public constructor(classValidatorError: ValidationError)
+    {
+        super();
+        this.message = "Internal objects failed validations."
+        this.name = "InternalValidationError";
+        this.classValidatorError = classValidatorError;
+    }
+}
 
 export class ExpressValidations
 {
-    public static async validateBodyAgainstModel<T extends object>(modelClass: { new() : T }, classObj: T)
+    public static async validateBodyAgainstModel<T extends object>(modelClass: ClassConstructor<T>, bodyObject: object)
     {
-        const modelInstance = new modelClass();
-        for (let key of Object.keys(classObj)) modelInstance[key] = key;
-        const validationErrors = await validate(modelInstance, 
-        {
-            forbidNonWhitelisted: true
-        });
+        const transformedObject = plainToInstance(modelClass, bodyObject);
+        const validationErrors = await validate(transformedObject);
         if (validationErrors[0])
             throw validationErrors[0];
     }
