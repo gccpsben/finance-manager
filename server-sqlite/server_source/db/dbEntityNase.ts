9fc8235a4caffdd6212296392aa4b7b0ff4ae917
gccpsben <gccpsben@gmail.com>
2024-07-24 17:21:13 +0800

    Added stack trace to errors logged to file

--- server-sqlite/server_source/db/dbEntityNase.ts
+++ server-sqlite/server_source/db/dbEntityNase.ts
@@ -16,7 +16,9 @@ export class EntityClass
             const internalValidationError = new InternalValidationError(errors[0]);
             const msgUUID = randomUUID();
             const consoleMsg = `One of the EntityClass failed internal validator. See log files for more info. (ErrorRefNo: ${msgUUID})`;
-            const logFileMsg = `One of the EntityClass failed internal validator (ErrorRefNo: ${msgUUID}).\n${JSON.stringify(internalValidationError, null, 4)}`;
+            const logFileMsg = `One of the EntityClass failed internal validator (ErrorRefNo: ${msgUUID}).`
+            + `\n${JSON.stringify(internalValidationError, null, 4)}`
+            + `\nAbove error stack trace: ${internalValidationError.stack}`;
             ExtendedLog.logRed(consoleMsg, false, true);
             ExtendedLog.logRed(logFileMsg, true, false);
             throw internalValidationError;
