53c765d8eb5a2288ec664387070a0e4d8315a7f4
gccpsben <gccpsben@gmail.com>
2024-07-26 15:30:04 +0800

    Added unit test template

---
+++ server-sqlite/tests/.index.test.js
@@ -0,0 +1,4 @@
+// This file only works when run with mocha cli.
+// You should run "npm run test" to unit test the backend.
+import "./scope1.test.js";
+import "./scope2.test.js";
---
+++ server-sqlite/tests/scope1.test.js
@@ -0,0 +1,18 @@
+export {};
+
+import chaiModule from "chai";
+import chaiHttp from "chai-http";
+const chai = chaiModule.use(chaiHttp);
+
+describe("Testing Scope 1" , () => 
+{
+    it("Testing Test 1", () => 
+    {
+        chai.assert.typeOf(1, 'number');
+    });
+
+    it("Testing Test 2", () => 
+    {
+        chai.assert.typeOf(1, 'string');
+    });
+});    
