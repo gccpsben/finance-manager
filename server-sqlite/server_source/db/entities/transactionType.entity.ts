4a9deb951ed8eaafcbc4c019739c81c68b26ed05
gccpsben <gccpsben@gmail.com>
2024-08-03 03:35:25 +0800

    Removed unused imports

--- server-sqlite/server_source/db/entities/transactionType.entity.ts
+++ server-sqlite/server_source/db/entities/transactionType.entity.ts
@@ -1,6 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate, OneToOne, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import "reflect-metadata"
-import { IsNotEmpty, IsString, MaxLength, validate } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
 import { EntityClass } from "../dbEntityBase.js";
 import { EnsureNotPlainForeignKey } from "../validators.js";
 import { User } from "./user.entity.js";
