e1edcfc0dc48c9f621bb2ca7a31b1f782792b091
gccpsben <gccpsben@gmail.com>
2024-08-10 01:15:32 +0800

    Added VueDevTools plugin

--- frontend-sqlite/vite.config.ts
+++ frontend-sqlite/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path = require('path')
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(
 {
-    plugins: [vue()],
+    plugins: [vue(), vueDevTools()],
     resolve: 
     {
         alias: 
