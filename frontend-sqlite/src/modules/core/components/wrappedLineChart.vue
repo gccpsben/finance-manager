3bdcd7569d14de1c9f435fee70012ded0f409c09
gccpsben <gccpsben@gmail.com>
2024-08-15 00:06:11 +0800

    Extracted chart js vertline plugin into a new file

--- frontend-sqlite/src/modules/core/components/wrappedLineChart.vue
+++ frontend-sqlite/src/modules/core/components/wrappedLineChart.vue
@@ -1,38 +1,13 @@
 <template>
-    <LineChart :plugins="[vertLinePlugin]" ref="mainChartRef" :chartData="chartData" :options="chartOptions"/>
+    <LineChart :plugins="[createVertLinePlugin(lineColor ?? defaultLineColor)]" ref="mainChartRef" :chartData="chartData" :options="chartOptions"/>
 </template>
-
-<style lang="less" scoped>
-@import '@/modules/core/stylesheets/globalStyle.less';
-#graphPanel
-{
-    overflow:hidden;
-    .fullSize; .bg(@backgroundDark);
-    box-sizing:border-box;
-
-    .graphPanelTitle { text-align:start; color:gray; font-size:14px; .tight; display:inline; }
-    .variantSelector
-    {
-        & > h2:nth-child(2) { margin-left:5px; }
-        & > h2:nth-child(3) { margin-left:5px; }
-        & > h2:nth-child(4) { margin-left:5px; }
-        
-        .variantTab { cursor: pointer; }
-        .variantTab.selected { color:@focus;}
-    }
-    & /deep/ .variantSelectorTab
-    {
-        color:white; text-align:start; color:gray; font-size:26px;
-        .tight;
-    }
-}
-</style>
 
 <script lang="ts" setup>
 import { LineChart } from 'vue-chart-3';
-import { Chart, registerables, type ChartOptions, type ChartData, type Plugin } from "chart.js";
+import { Chart, registerables, type ChartOptions, type ChartData } from "chart.js";
 import { computed, ref } from 'vue';
 import { extractDatePart, extractTimePart } from '../utils/date';
+import { createVertLinePlugin } from '../utils/chartJsVertLinePlugin';
 
 const defaultLineColor = `rgb(75, 192, 192)`;
 
@@ -44,82 +19,6 @@ const props = defineProps<
     datums: { x:number, y:number }[],
     isXAxisEpoch?: boolean
 }>();
-
-let lastMouseLocation = { x:0, y:0 };
-let lastNearestDatumIndex = undefined as undefined | number;
-let lastNearestDatasetIndex = undefined as undefined | number;
-
-const vertLinePlugin: Plugin<'line', {}> = 
-(
-    {
-        id: 'vertLine',
-        beforeDraw: function(chart, args, options)
-        {
-            const context = chart.canvas.getContext('2d')!;
-            const chartBounds = chart.canvas.getBoundingClientRect();
-
-            // Check if mouse is within bound, return if not
-            const isMouseWithinChartArea = (() => 
-            {
-                const relativeX = lastMouseLocation.x - chartBounds.x - chart.chartArea.left;
-                const relativeY = lastMouseLocation.y - chartBounds.y - chart.chartArea.top;
-                const isWithinLeftBorder = relativeX >= 0;
-                const isWithinRightBorder = relativeX <= chart.chartArea.width;
-                const isWithinTopBorder = relativeY >= 0;
-                const isWithinBottomBorder = relativeY <= chart.chartArea.height;
-                return isWithinLeftBorder && isWithinRightBorder && isWithinTopBorder && isWithinBottomBorder;
-            })();
-
-            if (!isMouseWithinChartArea) return;
-
-            let beforeStrokeStyle = context.strokeStyle;
-            context.strokeStyle = props.lineColor ?? defaultLineColor;
-            context.beginPath();
-            context.moveTo(lastMouseLocation.x - chartBounds.x, chart.chartArea.top);
-            context.lineTo(lastMouseLocation.x - chartBounds.x, chart.chartArea.height + chart.chartArea.top);
-
-            if (lastNearestDatumIndex !== undefined && lastNearestDatasetIndex !== undefined)
-            {
-                const activeElements = [ { datasetIndex: lastNearestDatasetIndex, index: lastNearestDatumIndex } ];
-                if (chart.tooltip) chart.tooltip.setActiveElements(activeElements, { x: lastMouseLocation.x, y: lastMouseLocation.y });
-                chart.setActiveElements(activeElements);
-            }
-            else
-            {
-                if (chart.tooltip) chart.tooltip.setActiveElements([], { x: lastMouseLocation.x, y: lastMouseLocation.y });
-                chart.setActiveElements([]);
-            }
-
-            context.stroke();
-            context.closePath();
-            context.strokeStyle = beforeStrokeStyle;
-        },
-        afterInit: function(chart, args, options) 
-        {           
-            chart.canvas.onmouseleave = (e:MouseEvent) => 
-            {
-                lastMouseLocation.x = -1;
-                lastMouseLocation.y = -1;
-            };
-            chart.canvas.onmousemove = (e: MouseEvent) => 
-            {
-                const nearestMouseItems = chart.getElementsAtEventForMode
-                (
-                    e, 
-                    'index', 
-                    { intersect: false }, 
-                    true
-                );
-
-                lastMouseLocation.x = e.clientX;
-                lastMouseLocation.y = e.clientY;
-                lastNearestDatumIndex = nearestMouseItems[0]?.index;
-                lastNearestDatasetIndex = nearestMouseItems[0]?.datasetIndex;
-                chart.update();
-            };
-        }
-    }
-);
 
 const chartOptions = computed(() => 
 {
---
+++ frontend-sqlite/src/modules/core/utils/chartJsVertLinePlugin.ts
@@ -0,0 +1,82 @@
+import { type Plugin } from "chart.js";
+
+export function createVertLinePlugin(lineColor: string)
+{
+    let lastMouseLocation = { x:0, y:0 };
+    let lastNearestDatumIndex = undefined as undefined | number;
+    let lastNearestDatasetIndex = undefined as undefined | number;
+
+    const vertLinePlugin: Plugin<'line', {}> = 
+    (
+        {
+            id: 'vertLine',
+            beforeDraw: function(chart, args, options)
+            {
+                const context = chart.canvas.getContext('2d')!;
+                const chartBounds = chart.canvas.getBoundingClientRect();
+
+                // Check if mouse is within bound, return if not
+                const isMouseWithinChartArea = (() => 
+                {
+                    const relativeX = lastMouseLocation.x - chartBounds.x - chart.chartArea.left;
+                    const relativeY = lastMouseLocation.y - chartBounds.y - chart.chartArea.top;
+                    const isWithinLeftBorder = relativeX >= 0;
+                    const isWithinRightBorder = relativeX <= chart.chartArea.width;
+                    const isWithinTopBorder = relativeY >= 0;
+                    const isWithinBottomBorder = relativeY <= chart.chartArea.height;
+                    return isWithinLeftBorder && isWithinRightBorder && isWithinTopBorder && isWithinBottomBorder;
+                })();
+
+                if (!isMouseWithinChartArea) return;
+
+                let beforeStrokeStyle = context.strokeStyle;
+                context.strokeStyle = lineColor;
+                context.beginPath();
+                context.moveTo(lastMouseLocation.x - chartBounds.x, chart.chartArea.top);
+                context.lineTo(lastMouseLocation.x - chartBounds.x, chart.chartArea.height + chart.chartArea.top);
+
+                if (lastNearestDatumIndex !== undefined && lastNearestDatasetIndex !== undefined)
+                {
+                    const activeElements = [ { datasetIndex: lastNearestDatasetIndex, index: lastNearestDatumIndex } ];
+                    if (chart.tooltip) chart.tooltip.setActiveElements(activeElements, { x: lastMouseLocation.x, y: lastMouseLocation.y });
+                    chart.setActiveElements(activeElements);
+                }
+                else
+                {
+                    if (chart.tooltip) chart.tooltip.setActiveElements([], { x: lastMouseLocation.x, y: lastMouseLocation.y });
+                    chart.setActiveElements([]);
+                }
+
+                context.stroke();
+                context.closePath();
+                context.strokeStyle = beforeStrokeStyle;
+            },
+            afterInit: function(chart, args, options) 
+            {           
+                chart.canvas.onmouseleave = (e:MouseEvent) => 
+                {
+                    lastMouseLocation.x = -1;
+                    lastMouseLocation.y = -1;
+                };
+                chart.canvas.onmousemove = (e: MouseEvent) => 
+                {
+                    const nearestMouseItems = chart.getElementsAtEventForMode
+                    (
+                        e, 
+                        'index', 
+                        { intersect: false }, 
+                        true
+                    );
+
+                    lastMouseLocation.x = e.clientX;
+                    lastMouseLocation.y = e.clientY;
+                    lastNearestDatumIndex = nearestMouseItems[0]?.index;
+                    lastNearestDatasetIndex = nearestMouseItems[0]?.datasetIndex;
+                    chart.update();
+                };
+            }
+        }
+    );
+
+    return vertLinePlugin;
+}
