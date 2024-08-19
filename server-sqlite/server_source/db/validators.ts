8837d9ad367631e9c9227000744413e4c6253199
gccpsben <gccpsben@gmail.com>
2024-08-19 14:32:46 +0800

    Separated validators inner checkers

--- server-sqlite/server_source/db/validators.ts
+++ server-sqlite/server_source/db/validators.ts
@@ -1,4 +1,4 @@
-import { registerDecorator, ValidationOptions, ValidationArguments, isInt, isPositive, isNumber, isNumberString, IsNumberString } from 'class-validator';
+import { registerDecorator, ValidationOptions, ValidationArguments, isInt, isPositive, isNumber, isNumberString, IsNumberString, isString } from 'class-validator';
 import { Decimal } from 'decimal.js';
 
 export function EnsureNotPlainForeignKey(validationOptions?: ValidationOptions) 
@@ -24,6 +24,11 @@ export function EnsureNotPlainForeignKey(validationOptions?: ValidationOptions)
     };
 }
 
+export function isDecimalJSString(value: any)
+{
+    try { const test = new Decimal(value); return true; }
+    catch(e) { return false; }
+}
 export function IsDecimalJSString(validationOptions?: ValidationOptions) 
 {
     return function (object: Object, propertyName: string) 
@@ -40,18 +45,26 @@ export function IsDecimalJSString(validationOptions?: ValidationOptions)
             {
                 validate(value: any, args: ValidationArguments) 
                 {
-                    try
-                    {
-                        let test = new Decimal(value);
-                        return true;
-                    }
-                    catch(e) { return false; }
+                    if (!isDecimalJSString(value)) return false;
+                    return true;
                 },
             },
         });
     };
 }
 
+export function isUTCDateIntString(value: any)
+{
+    try
+    {
+        if (!isNumberString(value)) return false;
+        const parsed = parseFloat(value);
+        if (!isPositive(parsed)) return false;
+        if (!isInt(parsed)) return false;
+        return true;
+    }
+    catch(e) { return false; }
+}
 export function IsUTCDateIntString(validationOptions?: ValidationOptions) 
 {
     return function (object: Object, propertyName: string) 
@@ -68,21 +81,25 @@ export function IsUTCDateIntString(validationOptions?: ValidationOptions)
             {
                 validate(value: any, args: ValidationArguments) 
                 {
-                    try
-                    {
-                        if (!isNumberString(value)) return false;
-                        const parsed = parseFloat(value);
-                        if (!isPositive(parsed)) return false;
-                        if (!isInt(parsed)) return false;
-                        return true;
-                    }
-                    catch(e) { return false; }
+                    if (!isUTCDateIntString(value)) return false;
+                    return true;
                 },
             },
         });
     };
 }
 
+export function isUTCDateInt(value: any)
+{
+    try
+    {
+        if (!isNumber(value)) return false;
+        if (!isInt(value)) return false;
+        if (!isPositive(value)) return false;
+        return true;
+    }
+    catch(e) { return false; }
+}
 export function IsUTCDateInt(validationOptions?: ValidationOptions) 
 {
     return function (object: Object, propertyName: string) 
@@ -99,20 +116,24 @@ export function IsUTCDateInt(validationOptions?: ValidationOptions)
             {
                 validate(value: any, args: ValidationArguments) 
                 {
-                    try
-                    {
-                        if (!isNumber(value)) return false;
-                        if (!isInt(value)) return false;
-                        if (!isPositive(value)) return false;
-                        return true;
-                    }
-                    catch(e) { return false; }
+                    if (!isUTCDateInt(value)) return false;
+                    return true;
                 },
             },
         });
     };
 }
 
+export function isIntString(value: any)
+{
+    try
+    {
+        if (!isNumberString(value)) return false;
+        if (!isInt(parseFloat(value))) return false;
+        return true;
+    }
+    catch(e) { return false; }
+}
 export function IsIntString(validationOptions?: ValidationOptions) 
 {
     return function (object: Object, propertyName: string) 
@@ -129,13 +150,78 @@ export function IsIntString(validationOptions?: ValidationOptions)
             {
                 validate(value: any, args: ValidationArguments) 
                 {
-                    try
-                    {
-                        if (!isNumberString(value)) return false;
-                        if (!isInt(parseFloat(value))) return false;
-                        return true;
-                    }
-                    catch(e) { return false; }
+                    if (!isIntString(value)) return false;
+                    return true;
+                },
+            },
+        });
+    };
+}
+
+export function isStringToStringDict(value :any)
+{
+    try
+    {
+        if (value === undefined || value === null) return false;
+        if (typeof value !== 'object') return false;
+        if (Object.keys(value).some(x => !isString(x))) return false;
+        if (Object.values(value).some(x => !isString(x))) return false;
+        return true;
+    }
+    catch(e) { return false; }
+}
+export function IsStringToStringDict(validationOptions?: ValidationOptions) 
+{
+    return function (object: Object, propertyName: string) 
+    {
+        registerDecorator(
+        {
+            name: 'isStringToStringDict',
+            target: object.constructor,
+            propertyName: propertyName,
+            options: {
+                message: "Expected an object where key and value are both string."
+            },
+            validator: 
+            {
+                validate(value: any, args: ValidationArguments) 
+                {
+                    if (!isStringToStringDict(value)) return false;
+                    return true;
+                },
+            },
+        });
+    };
+}
+
+export function isStringToDecimalJSStringDict(value:any)
+{
+    try
+    {
+        if (!isStringToStringDict(value)) return false;
+        if (Object.values(value).some(x => !isDecimalJSString(x))) return false;
+        return true;
+    }
+    catch(e) { return false; }
+}
+export function IsStringToDecimalJSStringDict(validationOptions?: ValidationOptions) 
+{
+    return function (object: Object, propertyName: string) 
+    {
+        registerDecorator(
+        {
+            name: 'isStringToDecimalJSStringDict',
+            target: object.constructor,
+            propertyName: propertyName,
+            options: {
+                message: "Expected an object where keys are string, values are Decimal.js string"
+            },
+            validator: 
+            {
+                validate(value: any, args: ValidationArguments) 
+                {
+                    if (!isStringToDecimalJSStringDict(value)) return false;
+                    return true;
                 },
             },
         });
