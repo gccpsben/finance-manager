2a6f5c2f34f8662514d94f4cf6d54107a0952428
gccpsben <gccpsben@gmail.com>
2024-09-26 09:55:09 +0800

    Added manifest for PWAs

--- server/server_source/routes/staticAssets.ts
+++ server/server_source/routes/staticAssets.ts
@@ -3,7 +3,8 @@ import { distFolderLocation } from '../server';
 
 const router = express.Router();
 
-// router.use("/", express.static(distFolderLocation));
+router.get("/manifest.webmanifest", (req, res) => { res.sendFile(`manifest.json`, { root: distFolderLocation }); });
+router.get("/manifest.json", (req, res) => { res.sendFile(`manifest.json`, { root: distFolderLocation }); });
 router.get("/assets/*", (req, res) => { res.sendFile(req.path, { root: distFolderLocation }); });
 router.get("/*", (req, res) => { res.sendFile("index.html", { root: distFolderLocation }); });
 
