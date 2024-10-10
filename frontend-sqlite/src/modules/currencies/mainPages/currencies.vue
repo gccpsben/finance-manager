0c2b6a930a412671be57f012f8822567b2e2ee3a
gccpsben <gccpsben@gmail.com>
2024-10-11 01:00:35 +0800

    Currencies page max page size 10

--- frontend-sqlite/src/modules/currencies/mainPages/currencies.vue
+++ frontend-sqlite/src/modules/currencies/mainPages/currencies.vue
@@ -13,13 +13,14 @@
                                         v-model:model-value="mainPagination.currentPage.value"></NumberPagination>
                 </div>
                 <OverlapArea class="fullSize">
-                    <CustomTable class="allCurrenciesTable" rows="50px auto" columns="1fr" rowRows="1fr" :style="{opacity: containersStore.currencies.isLoading ? 0.3 : 1}"
-                                rowColumns="1fr auto auto" rowAreas="'name rateToBase rateHistory'" bodyRows="min-content">
+                    <CustomTable class="allCurrenciesTable" rows="50px auto" columns="1fr" rowRows="1fr"
+                                 :style="{opacity: currenciesStore.currencies.isLoading ? 0.3 : 1}"
+                                 rowColumns="1fr auto auto" rowAreas="'name rateToBase rateHistory'" bodyRows="min-content">
                         <template #header>
                             <CustomTableRow class="headerRow fullSize" style="font-weight: bold;">
                                 <CustomTableCell grid-area="name" class="yCenter xLeft">Name</CustomTableCell>
                                 <CustomTableCell grid-area="rateToBase" class="yCenter xRight">
-                                    Rate to {{ containersStore.getBaseCurrencySymbol() }}
+                                    Rate to {{ currenciesStore.getBaseCurrencySymbol() }}
                                 </CustomTableCell>
                                 <CustomTableCell grid-area="rateHistory" class="center">
                                     History 7d
@@ -46,8 +47,8 @@
                             </CustomTableRow>
                         </template>
                     </CustomTable>
-                    <NetworkCircularIndicator :error="containersStore.currencies.error"
-                                            :is-loading="containersStore.currencies.isLoading"
+                    <NetworkCircularIndicator :error="currenciesStore.currencies.error"
+                                            :is-loading="currenciesStore.currencies.isLoading"
                                             style="pointer-events: none;"/>
                 </OverlapArea>
             </div>
@@ -59,7 +60,7 @@
 import CustomTable from '@/modules/core/components/tables/customTable.vue';
 import CustomTableCell from '@/modules/core/components/tables/customTableCell.vue';
 import CustomTableRow from '@/modules/core/components/tables/customTableRow.vue';
-import {  watch } from 'vue';
+import { watch } from 'vue';
 import { useCurrenciesStore } from '../stores/useCurrenciesStore';
 import useNetworkPaginationNew, { type UpdaterReturnType } from '@/modules/core/composables/useNetworkedPagination';
 import type { CurrencyDTO } from '../../../../../api-types/currencies';
@@ -68,14 +69,14 @@ import OverlapArea from '@/modules/core/components/overlapArea.vue';
 import NetworkCircularIndicator from '@/modules/core/components/networkCircularIndicator.vue';
 import CurrencyRateHistoryThumbnail from '../components/currencyRateHistoryThumbnail.vue';
 
-const containersStore = useCurrenciesStore();
-containersStore.currencies.updateData();
+const currenciesStore = useCurrenciesStore();
+currenciesStore.currencies.updateData();
 const mainPagination = useNetworkPaginationNew<CurrencyDTO>(
 {
     updater: async (start:number, end:number): Promise<UpdaterReturnType<CurrencyDTO>> =>
     {
-        await containersStore.currencies.updateData();
-        const currencies = containersStore.currencies.lastSuccessfulData?.rangeItems ?? [];
+        await currenciesStore.currencies.updateData();
+        const currencies = currenciesStore.currencies.lastSuccessfulData?.rangeItems ?? [];
         const endIndex = Math.min(currencies.length, end);
 
         return {
@@ -86,7 +87,7 @@ const mainPagination = useNetworkPaginationNew<CurrencyDTO>(
         };
     },
     pageIndex: 0,
-    pageSize: 15,
+    pageSize: 10,
     overflowResolutionHandler: (_, lastAvailablePageIndex) => mainPagination.currentPage.value = lastAvailablePageIndex,
     updateOnMount: true
 });
