70c23ae9ac2116cc302a9fc3fdbe649ce98f480a
gccpsben <gccpsben@gmail.com>
2024-10-10 22:14:02 +0800

    Fixed loading icon not overlapping list cell

--- frontend-sqlite/src/modules/core/components/listCell.vue
+++ frontend-sqlite/src/modules/core/components/listCell.vue
@@ -3,30 +3,30 @@
     <cell :title="title.get()" :inset-mode="props.insetMode">
         <template #cellOptions>
             <div class="pageSelector">
-                <numberPagination v-model="currentPageIndex" :min-page-readable="1" 
+                <numberPagination v-model="currentPageIndex" :min-page-readable="1"
                                   :max-page-readable="maxPageAllowed + 1" />
             </div>
         </template>
-        <grid-shortcut style="overflow: hidden; grid-auto-rows: 1fr;" columns="1fr" class="fullSize">
-            <NetworkCircularIndicator v-if="isLoading.get()||error.get()" 
-                                      :isLoading="isLoading.get()" 
-                                      :error="error.get()" 
-                                      class="fullHeight"
-            />
-            <div v-else-if="currentViewItems.length == 0" class="fullSize center"> 
-                {{ noItemsText.get() }} 
-            </div>
-            <div v-for="currentItem in currentViewItems">
-                <slot name="row" :currentItem="currentItem"></slot>
-            </div>
-            <!-- Automatically add empty rows if the current page hasn't enough items -->
-            <div v-if="currentViewItems.length > 0" v-for="blankRow in new Array(itemsInPage.get()! - currentViewItems.length)">
-
-            </div>
-        </grid-shortcut>
+        <OverlapArea class="fullHeight">
+            <NetworkCircularIndicator v-if="isLoading.get()||error.get()"
+                                  :isLoading="isLoading.get()"
+                                  :error="error.get()"/>
+            <grid-shortcut :style="{opacity: isLoading.get() ? 0.5 : 1}" columns="1fr"
+                           style="overflow: hidden; grid-auto-rows: 1fr;" class="fullSize">
+                <div v-if="currentViewItems.length == 0" class="fullSize center">
+                    {{ noItemsText.get() }}
+                </div>
+                <div v-for="currentItem in currentViewItems">
+                    <slot name="row" :currentItem="currentItem"></slot>
+                </div>
+                <!-- Automatically add empty rows if the current page hasn't enough items -->
+                <div v-if="currentViewItems.length > 0"
+                     v-for="blankRow in new Array(itemsInPage.get()! - currentViewItems.length)"></div>
+            </grid-shortcut>
+        </OverlapArea>
     </cell>
 
-</template> 
+</template>
 
 <script lang="ts" setup generic="T">
 import NetworkCircularIndicator from '@/modules/core/components/networkCircularIndicator.vue';
@@ -34,6 +34,7 @@ import { computed, ref } from 'vue';
 import { defineProperty } from '../utils/defineProperty';
 import cell from '@/modules/core/components/cell.vue';
 import numberPagination from '@/modules/core/components/numberPagination.vue';
+import OverlapArea from './overlapArea.vue';
 
 const props = withDefaults(defineProps<
 {
@@ -44,7 +45,7 @@ const props = withDefaults(defineProps<
     isLoading?: boolean,
     error?: object,
     insetMode?: boolean
-}>(), 
+}>(),
 {
     itemsInPage: 7,
     error: undefined,
@@ -55,21 +56,21 @@ const props = withDefaults(defineProps<
     insetMode: false
 });
 
-const items = defineProperty<T[], 'items', typeof props>("items", { emitFunc: undefined, props: props, withEmits: false });
-const itemsInPage = defineProperty<number, 'itemsInPage', typeof props>("itemsInPage", { emitFunc: undefined, props: props, withEmits: false });
-const title = defineProperty<string, 'title', typeof props>("title", { emitFunc: undefined, props: props, withEmits: false });
-const noItemsText = defineProperty<string, 'noItemsText', typeof props>("noItemsText", { emitFunc: undefined, props: props, withEmits: false });
-const isLoading = defineProperty<boolean, 'isLoading', typeof props>("isLoading", { emitFunc: undefined, props: props, withEmits: false });
-const error = defineProperty<object|undefined, 'error', typeof props>("error", { emitFunc: undefined, props: props, withEmits: false });
+const items = defineProperty<T[], 'items', typeof props>("items", { default: [], emitFunc: undefined, props: props });
+const itemsInPage = defineProperty<number, 'itemsInPage', typeof props>("itemsInPage", { default: 7, emitFunc: undefined, props: props });
+const title = defineProperty<string, 'title', typeof props>("title", { default: "Title Here", emitFunc: undefined, props: props });
+const noItemsText = defineProperty<string, 'noItemsText', typeof props>("noItemsText", { default: "No Items", emitFunc: undefined, props: props });
+const isLoading = defineProperty<boolean, 'isLoading', typeof props>("isLoading", { default: true, emitFunc: undefined, props: props });
+const error = defineProperty<object|undefined, 'error', typeof props>("error", { default: undefined, emitFunc: undefined, props: props });
 const currentPageIndex = ref(0);
 const maxPageAllowed = computed(() => Math.floor((items.get().length - 1) / itemsInPage.get()!));
-const currentViewItems = computed(() => 
+const currentViewItems = computed(() =>
 {
     if (items.get()!.length == 0) return [];
     if (items.get()!.length <= itemsInPage.get()!) return items.get();
     let lowerBoundIndex = currentPageIndex.value * itemsInPage.get()!;
     let upperBoundIndex = lowerBoundIndex + itemsInPage.get()!;
     if (upperBoundIndex >= items.get()!.length) upperBoundIndex = items.get()!.length;
-    return (items.get()).slice(lowerBoundIndex, upperBoundIndex);    
+    return (items.get()).slice(lowerBoundIndex, upperBoundIndex);
 });
 </script>
