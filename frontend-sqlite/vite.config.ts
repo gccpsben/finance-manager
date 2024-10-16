15ac7bd8ff6aaa78f3c18c7137c3dc31111cc669
gccpsben <gccpsben@gmail.com>
2024-10-16 10:05:03 +0800

    Fixed vite config

--- frontend-sqlite/vite.config.ts
+++ frontend-sqlite/vite.config.ts
@@ -1,42 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path = require('path')
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig(
 {
-    plugins: [vue(), vueDevTools()],
-    resolve: 
+	plugins: [ vue() ],
+    resolve:
     {
-        alias: 
-        {
-            '@': path.resolve(__dirname, './src'),
-        }
+        alias: { '@': path.resolve(__dirname, './src') }
     },
     server:
 	{
 		proxy:
 		{
-			'/api': 
+			'/api':
 			{
-				target: 'https://192.168.8.130:8081',
+				target: 'https://localhost:8081',
 				changeOrigin: true,
 				secure: false
 			},
-			'/socket.io': {
-				target: 'https://192.168.8.130:8081',
+			'/socket.io':
+			{
+				target: 'https://localhost:8081',
 				changeOrigin: true,
 				ws: true,
 				secure: false
 			},
-            "/*": {
-				target: 'https://192.168.8.130:8081'
-			},
+            "/*": { target: 'https://localhost:8081' }
 		}
 	},
-    build:
-    {
-        outDir: "../dist"
-    }
+    build: { outDir: "../dist" }
 })
