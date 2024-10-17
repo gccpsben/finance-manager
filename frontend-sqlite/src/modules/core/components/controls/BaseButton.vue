729ba773e306e8e7bbcd6275cc3f9685c4dd4f06
gccpsben <gccpsben@gmail.com>
2024-10-17 12:19:15 +0800

    Added BaseButton and LeftRightGrid

---
+++ frontend-sqlite/src/modules/core/components/controls/BaseButton.vue
@@ -0,0 +1,58 @@
+<template>
+    <button>
+        <div class="baseBtnInner" :class="{'noIcon': !icon}">
+            <div class="center fullSize" v-if="icon">
+                <GaIcon :style="{ fontSize: iconSize }" :icon="icon"/>
+            </div>
+            <div class="center fullSize" style="transform: translateY(1px)">
+                <slot />
+            </div>
+        </div>
+    </button>
+</template>
+
+<script setup lang="ts">
+import GaIcon from '../gaIcon.vue';
+
+export type BaseButtonProps =
+{
+    iconSize?: string;
+    gap?: string;
+    icon?: string | undefined;
+    fontSize?: string;
+};
+withDefaults
+(
+    defineProps<BaseButtonProps>(),
+    { iconSize: "16px", gap: "4px", fontSize: "12px" }
+)
+</script>
+
+<style lang="less" scoped>
+@import '@/modules/core/stylesheets/globalStyle.less';
+
+button
+{
+    .minButtonBase;
+    background: transparent;
+    border: 1px solid @border;
+    color: @foreground;
+    padding:10px;
+    border-radius: 5px;
+    &:hover:not(:disabled) { background: fade(@focus, 10%); }
+    &:disabled { opacity: 0.5; user-select: none; cursor:not-allowed; }
+
+    padding:5px;
+    font-size:v-bind(fontSize);
+    font-weight: bold;
+
+    .baseBtnInner
+    {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        grid-template-rows: 1fr;
+        gap: v-bind(gap);
+        &.noIcon { gap: 0px; }
+    }
+}
+</style>
---
+++ frontend-sqlite/src/modules/core/components/layout/LeftRightGrid.vue
@@ -0,0 +1,28 @@
+<template>
+    <div style="display: grid; grid-template-rows: 1fr;" :style="{gridTemplateColumns: computedColumns}">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export type LeftRightGridProps =
+{
+    columnWidthConfig: "AUTO" | "EQUAL" | "LEFT_AUTO" | "RIGHT_AUTO";
+};
+
+const props = defineProps<LeftRightGridProps>();
+const computedColumns = computed(() =>
+{
+    const columnWidthConfig = props.columnWidthConfig;
+    if (columnWidthConfig === 'AUTO')
+        return "auto auto";
+    if (columnWidthConfig === 'EQUAL')
+        return '1fr 1fr';
+    if (columnWidthConfig === 'LEFT_AUTO')
+        return 'auto 1fr';
+    if (columnWidthConfig === 'RIGHT_AUTO')
+        return '1fr auto';
+});
+</script>
