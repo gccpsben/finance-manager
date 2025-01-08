7018d07c5731021e7c61de72d2033f160b127c1c
gccpsben <gccpsben@gmail.com>
2025-01-08 23:19:44 +0800

    Make attachments field responsive to size

--- frontend-sqlite/src/modules/core/components/data-display/AttachmentsField.vue
+++ frontend-sqlite/src/modules/core/components/data-display/AttachmentsField.vue
@@ -5,7 +5,7 @@
             <template #content>
                 <AbsEnclosure v-if="props.files.length > 0">
                     <div style="overflow-y: scroll; padding: 14px; padding-top: 0px; transform: translateY(15px); height: calc(100% - 15px);" class="fullSize">
-                        <div style="display: grid; grid-template-columns: 1fr 1fr; grid-auto-rows: 45px; gap: 8px; margin-bottom: 6px;">
+                        <div id="boxesContainer">
                             <AttachmentBox v-for="file in props.files"
                                            :extension="file.extension" :file-name="file.fileName" />
                         </div>
@@ -47,6 +47,8 @@ const props = defineProps<AttachmentsBoxPropsType>();
 .attachmentsFieldRoot
 {
     box-shadow: 5px black inset;
+    container-name: attachmentsFieldRoot;
+    container-type: size;
 
     .noItemsContainer
     {
@@ -58,6 +60,23 @@ const props = defineProps<AttachmentsBoxPropsType>();
             gap: 14px;
             font-size: 12px;
         }
+    }
+}
+
+#boxesContainer
+{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 45px;
+    gap: 8px;
+    margin-bottom: 6px;
+}
+
+@container attachmentsFieldRoot (width <= 600px)
+{
+    #boxesContainer
+    {
+        grid-template-columns: 1fr;
     }
 }
 </style>
