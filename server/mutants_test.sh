21239846745cf4a359452d5c5dd8357a3ca722dc
gccpsben <gccpsben@gmail.com>
2025-03-30 02:44:19 +0800

    skip ts-rs in mutant tests

--- server/mutants_test.sh
+++ server/mutants_test.sh
@@ -1,4 +1,5 @@
 # We use only 1 thread because the tests are done against a real database.
 # the test will read TEST_DB_URL_0 for each test.
 # If multiple threads are used, TEST_DB_URL_1, TEST_DB_URL_2 etc... are used.
-cargo mutants --test-tool=nextest --in-place -- --test-threads=1
+# "-- --skip export_bindings_" also disable typescript definitions exports.
+cargo mutants --test-tool=nextest --in-place -- --test-threads=1 -- --skip export_bindings_
